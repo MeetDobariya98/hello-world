7d3ed5c2a1a746b86ec3ffc76bf4f7696e585eff
Meet Dobariya <163340152+MeetDobariya98@users.noreply.github.com>
2025-08-22 15:57:11 +0530

    Update ch2pracqu1.js

--- ch2pracqu1.js
+++ ch2pracqu1.js
@@ -1,12 +1,13 @@
-//get user to print input a number usinf prompt("enter number: "). cheak if the number is a multiple of 5 or not.
+//get user to print input a number usinf prompt("enter number: "). cheak if the number is a multiple of 2 or not.
 
 let num = prompt("enter number");
 
-if(num % 5 == 0)
+if(num % 2 == 0)
 {
-    console.log(num,"is multiple by 5");
+    console.log(num,"is multiple by 2");
 }
 else
 {
-    console.log(num,"is not multiple by 5");
+    console.log(num,"is not multiple by 2");
+
 }
