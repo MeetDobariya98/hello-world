7a4c0c5d1d01aeb2e09544fec50d33927b599249
Meet Dobariya <163340152+MeetDobariya98@users.noreply.github.com>
2025-08-22 15:56:03 +0530

    Add files via upload

---
+++ ch2pracqu1.js
@@ -0,0 +1,12 @@
+//get user to print input a number usinf prompt("enter number: "). cheak if the number is a multiple of 5 or not.
+
+let num = prompt("enter number");
+
+if(num % 5 == 0)
+{
+    console.log(num,"is multiple by 5");
+}
+else
+{
+    console.log(num,"is not multiple by 5");
+}
